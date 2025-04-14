72a80db07f2a569aa6ec1e92bfecda770d5f1c47
shorouk589 <shoroukashraf66@gmail.com>
2025-04-14 23:06:09 +0200

    Specification Fun

--- Infrastructure/Persistence/Repositories/GenericRepository.cs
+++ Infrastructure/Persistence/Repositories/GenericRepository.cs
@@ -66,8 +66,21 @@ namespace Persistence.Repositories
             _context.Remove(entity);
         }
 
+        public async Task<IEnumerable<TEntity>> GetAllAsync(ISpecifications<TEntity, TKey> spec, bool trackChanges = false)
+        {
+         return await ApplySpecification(spec).ToListAsync();
+          
+        }
+
+        public async Task<TEntity?> GetAsync(ISpecifications<TEntity, TKey> spec, TKey id)
+        {
+            return await ApplySpecification(spec).FirstOrDefaultAsync();
+        }
 
 
-
+        private IQueryable<TEntity> ApplySpecification( ISpecifications<TEntity, TKey> spec)
+        {
+            return SpecificationEvaluator.GetQuEry(_context.Set<TEntity>(),spec);
+        }
     }
 }
